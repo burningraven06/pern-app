9911a1171f397a679db4c7fe19bb3cb5051e8fc2
Toufiq <burningraven06@gmail.com>
2018-03-05 18:53:58 +0600

    make second api call, write second apicall function, render data

--- react_node_express_app/src/App.js
+++ react_node_express_app/src/App.js
@@ -2,26 +2,49 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    myMsg: ""
+    myMsg: "",
+    myCars: []
   }
   componentDidMount(){
-    this.callApi().then(res => this.setState({ myMsg: res.backMsg})).catch(err => console.log(err));
+    this.callApiDefault().then(res => this.setState({ myMsg: res.backMsg})).catch(err => console.log(err));
+
+    this.callApiCars().then(res => this.setState({ myCars: res.backCars})).catch(err => console.log(err));
   }
 
-  callApi = async() =>{
+  callApiDefault = async() =>{
     const response = await fetch('/api/hello');
     const body = await response.json();
     
-    if (response.status !=200) throw Error(body.message);
+    if (response.status !==200) throw Error(body.message);
+    // console.log("Api Call Successfull, Data: ", body);
 
     return body;
   };
+
+  callApiCars = async() =>{
+    const response = await fetch('/api/cars');
+    const resBody = await response.json();
+    if (response.status !== 200) throw Error(resBody.message);
+    return resBody;
+  }
 
   render() {
     return (
       <div className="App">
         <h1> React App </h1>
         <h4> {this.state.myMsg} </h4>
+  
+          <ul>
+            {
+              this.state.myCars.map((car) => (
+                <li key={car.id}>
+                  <strong style={{color:car.color}}>{car.name} </strong> {car.color} {car.price}
+                </li>
+              )) 
+            }
+          </ul>
+    
+         
       </div>
     );
   }
