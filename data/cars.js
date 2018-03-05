9d294f1f5272d802242c81898da0d60e117a519f
Toufiq <burningraven06@gmail.com>
2018-03-05 18:51:49 +0600

    create dummy data to send from backend

---
+++ data/cars.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const sampleCarArray = [
+      {
+         id: 1,
+         name: "Totyota",
+         color: "Red",
+         price:1250
+      },
+      {
+         id: 2,
+         name: "BMW",
+         color: "Green",
+         price: 125000
+      },
+      {
+         id: 3,
+         name: "Audi",
+         color: "Black",
+         price: 50000
+      },
+   ]
+module.exports = sampleCarArray;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3300;
+const sampleCarArray = require('./data/cars');
 
 app.get('/api/hello', (req, res) => {
-   res.status(200).send({backMsg: 'App Works'});
+   res.status(200).send({backMsg: 'Express App Works'});
+});
+
+app.get('/api/cars', (req, res) =>{
+   res.status(200).send({backCars: sampleCarArray});
 });
 
 app.listen(port, () =>{
