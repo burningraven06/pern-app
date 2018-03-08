71c0bc2045986649ae2485a7c60dbf52dd1b2e7a
Toufiq <burningraven06@gmail.com>
2018-03-08 18:10:17 +0600

    create second route

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3300;
 const sampleCarArray = require('./data/cars');
-
-app.get('/api/hello', (req, res) => {
-   res.status(200).send({backMsg: 'Express App Works'});
-});
-
-app.get('/api/cars', (req, res) =>{
-   res.status(200).send({backCars: sampleCarArray});
-});
+const sampleFruitArray = require('./data/fruits');
 
 app.listen(port, () =>{
    console.log(`Express running on port: ${port}`);
+});
+
+app.get('/api/hello', (req, res) => {
+   res.status(200).send({ backMsg: 'Express App Works' });
+});
+
+app.get('/api/cars', (req, res) => {
+   res.status(200).send({ backCars: sampleCarArray });
+});
+
+app.get('/api/fruits', (req, res) => {
+   res.status(200).send({backFruits: sampleFruitArray });
 })
