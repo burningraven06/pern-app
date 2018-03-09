9dc0691a5be1576ec72cd0efc27dab1609127b1b
Toufiq <burningraven06@gmail.com>
2018-03-09 14:08:23 +0600

    pass props through routes

--- react_node_express_app/src/components/msgComp.js
+++ react_node_express_app/src/components/msgComp.js
@@ -1,26 +1,30 @@
 import React from 'react';
 
-// class MessageComp extends React.Component{
-//    constructor(props){
-//       super(props);
-// 	}
+export default class MessageComp extends React.Component{
+   constructor(props){
+      super(props);
+		this.state = {
+			myMsg: props.theMsg
+		}
+	}
+
+	componentDidMount(){
+		// this.callApiMsg().then(res => this.setState({ myMsg: res.backMsg })).catch(err => console.log(err));
+	}
+
+	callApiMsg = async () => {
+		const response = await fetch('/api/hello');
+		const body = await response.json();
+		if (response.status !== 200) throw Error(body.message);
+		return body;
+	};
 	
-// 	render(){
-// 		const passedVal = this.props;
-
-//      	return (
-//        	<div>
-// 				<p> Hey {passedVal.msgText}</p>
-// 			</div>
-//      	);	
-//   	}
-// }
-
-// export default MessageComp;
-
-const MessageComp = (props) => {
-	return (
-		<p> Hey! {props.msgText} </p>
-	)
+	render(){
+	
+     	return (
+       	<div>
+				<p> Hey {this.state.myMsg}</p>
+			</div>
+     	);	
+  	}
 }
-export default MessageComp;
