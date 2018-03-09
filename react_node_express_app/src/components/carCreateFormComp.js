81c1a9bf648a7ec3ec58759fb86dfa9677736a26
Toufiq <burningraven06@gmail.com>
2018-03-09 23:52:07 +0600

    tried a lot for post request

---
+++ react_node_express_app/src/components/carCreateFormComp.js
@@ -0,0 +1,44 @@
+import React from 'react';
+export default class CarCreateComp extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         carname: "",
+         carcolor: "",
+         carprice: ""
+      }
+      this.handleCarNameChange = this.handleCarNameChange.bind(this);
+      this.handleCarColorChange = this.handleCarColorChange.bind(this);
+      this.handleCarPriceChange = this.handleCarPriceChange.bind(this);
+      this.unRenderFormComp = this.unRenderFormComp.bind(this);
+ 
+   }
+   handleCarNameChange = (event) => {
+      this.setState({ carname: event.target.value });
+   }
+   handleCarColorChange = (event) => {
+      this.setState({ carcolor: event.target.value });
+   }
+   handleCarPriceChange = (event) => {
+      this.setState({ carprice: event.target.value });
+   }
+
+   unRenderFormComp = (event) => {
+      event.preventDefault()
+      // console.log(this.state.carname, this.state.carcolor, this.state.carprice);
+      this.props.unRenderComp(this.state.carname, this.state.carcolor, this.state.carprice);
+   }
+
+   render(){
+      return (
+         <div className='col-sm-6 col-sm-offset-3'> 
+            <form className='form' onSubmit={this.handleSubmit}> 
+               <input type='text' placeholder='Name' name='carname' className='form-control' onChange={this.handleCarNameChange} />
+               <input type='text' placeholder='Color' name='carcolor' className='form-control' onChange={this.handleCarColorChange} />
+               <input type='text' placeholder='Price' name='carprice' className='form-control' onChange={this.handleCarPriceChange} />
+               <button type='submit' className='btn btn-primary' onClick={this.unRenderFormComp}> Submit </button>
+            </form>
+         </div>
+      );
+   }
+}
