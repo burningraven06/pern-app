d7b1e1c247870e93e46d46e5e3e332d07b6f5896
Toufiq <burningraven06@gmail.com>
2018-03-09 16:51:38 +0600

    install body-parser

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
 const carRouter = require('./routes/carRoutes');
 const fruitRouter = require('./routes/fruitRoutes');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () =>{
    console.log(`Express running on port: ${port}`);
