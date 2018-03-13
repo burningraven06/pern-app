4fd5af23984cc50dcca2e8b89086881be2bb958e
Toufiq <burningraven06@gmail.com>
2018-03-13 23:30:21 +0600

    create form and first input with handle change

--- react_node_express_app/src/components/carProfileComp.js
+++ react_node_express_app/src/components/carProfileComp.js
@@ -3,98 +3,98 @@ import './car.css';
 import axios from 'axios';
 
 class CarProfileComp extends React.Component{
-   constructor(props){
-      super(props);
-      this.state = {
-         theSingleCar: {},
-         isEditing: false,
-         editedCarName: "",
-         editedCarColor: "",
-         editedCarPrice: "",
-         nameInValid: false,
-         colorInValid: false,
-         priceInValid: false,
-      }
-      this.editModeOn = this.editModeOn.bind(this)
-      this.editModeOff = this.editModeOff.bind(this)
-      this.handleCarNameChange = this.handleCarNameChange.bind(this)
-      this.handleCarColorChange = this.handleCarColorChange.bind(this)
-      this.handleCarPriceChange = this.handleCarPriceChange.bind(this)
-      this.updateCar = this.updateCar.bind(this)
-      this.validateFormData = this.validateFormData.bind(this)
-   }
+  constructor(props){
+    super(props);
+    this.state = {
+			theSingleCar: {},
+			isEditing: false,
+			editedCarName: "",
+			editedCarColor: "",
+			editedCarPrice: "",
+			nameInValid: false,
+			colorInValid: false,
+			priceInValid: false,
+		}
+		this.editModeOn = this.editModeOn.bind(this)
+		this.editModeOff = this.editModeOff.bind(this)
+		this.handleCarNameChange = this.handleCarNameChange.bind(this)
+		this.handleCarColorChange = this.handleCarColorChange.bind(this)
+		this.handleCarPriceChange = this.handleCarPriceChange.bind(this)
+		this.updateCar = this.updateCar.bind(this)
+		this.validateFormData = this.validateFormData.bind(this)
+	}
    
-   componentDidMount(){
-      this.callApiGetSingleCar().then( res => this.setState({
-         theSingleCar: res.singleCar, 
-      })).catch( err => console.log(err));
-   }
+	componentDidMount(){
+		this.callApiGetSingleCar().then( res => this.setState({
+			theSingleCar: res.singleCar, 
+		})).catch( err => console.log(err));
+	}
 
-   componentDidUpdate(){
-      this.callApiGetSingleCar().then( res => this.setState({
-         theSingleCar: res.singleCar,
-      })).catch( err => console.log(err));
-   }
+  componentDidUpdate(){
+    this.callApiGetSingleCar().then( res => this.setState({
+      theSingleCar: res.singleCar,
+    })).catch( err => console.log(err));
+  }
 
-   callApiGetSingleCar = async() => {
-      const fetchURL = '/api/cars/' + this.props.match.params.id
-      const response = await fetch(fetchURL);
-      const resBody = await response.json();
-      if (response.status !== 200) throw Error(resBody.message);
-      return resBody;
-   }
+  callApiGetSingleCar = async() => {
+    const fetchURL = '/api/cars/' + this.props.match.params.id
+    const response = await fetch(fetchURL);
+    const resBody = await response.json();
+    if (response.status !== 200) throw Error(resBody.message);
+    return resBody;
+  }
 
-   editModeOn = () => {
-      this.setState({ isEditing: true, editedCarName: this.state.theSingleCar.name,
-         editedCarColor: this.state.theSingleCar.color, editedCarPrice: this.state.theSingleCar.price})
-   }
+  editModeOn = () => {
+		this.setState({ isEditing: true, editedCarName: this.state.theSingleCar.name,
+				editedCarColor: this.state.theSingleCar.color, editedCarPrice: this.state.theSingleCar.price})
+  }
 
-   editModeOff = () => {
-      this.setState({ isEditing: false})
-   }
+	editModeOff = () => {
+		this.setState({ isEditing: false})
+	}
 
-   handleCarNameChange = (event) => {
-      this.setState({ editedCarName: event.target.value})
-   }
+	handleCarNameChange = (event) => {
+		this.setState({ editedCarName: event.target.value})
+	}
 
-   handleCarColorChange = (event) => {
-      this.setState({ editedCarColor: event.target.value})
-   }
+	handleCarColorChange = (event) => {
+		this.setState({ editedCarColor: event.target.value})
+	}
+	
+	handleCarPriceChange = (event) => {
+		this.setState({ editedCarPrice: event.target.value})
+	}
+
+	validateFormData = () => {
+		if (this.state.editedCarName.length > 0 && this.state.editedCarColor.length > 0 &&  this.state.editedCarPrice ){
+			return true;
+		}
+		if (!this.state.editedCarName.length > 0) {
+				this.setState({ nameInValid: true })
+			document.getElementById('carNameInput').className += ' orange-boundary';
+			return false;
+		}
+		if (!this.state.editedCarColor.length > 0) {
+				this.setState({ colorInValid: true });
+			document.getElementById('carColorInput').className += ' orange-boundary';
+			return false;
+		}
+		if (!this.state.editedCarPrice) {
+				this.setState({ priceInValid: true });
+			document.getElementById('carPriceInput').className += ' orange-boundary';
+			return false;
+		}
+	}
    
-   handleCarPriceChange = (event) => {
-      this.setState({ editedCarPrice: event.target.value})
-   }
+  resetValidationCSS = () => {
+		this.setState({ nameInValid: false, colorInValid: false, priceInValid: false})
+		document.getElementById('carPriceInput').className -= ' orange-boundary';
+		document.getElementById('carColorInput').className -= ' orange-boundary';
+		document.getElementById('carPriceInput').className -= ' orange-boundary';
+  }
 
-   validateFormData = () => {
-      if (this.state.editedCarName.length > 0 && this.state.editedCarColor.length > 0 &&  this.state.editedCarPrice ){
-         return true;
-      }
-      if (!this.state.editedCarName.length > 0) {
-         this.setState({ nameInValid: true })
-         document.getElementById('carNameInput').className += ' orange-boundary';
-         return false;
-      }
-      if (!this.state.editedCarColor.length > 0) {
-         this.setState({ colorInValid: true });
-         document.getElementById('carColorInput').className += ' orange-boundary';
-         return false;
-      }
-      if (!this.state.editedCarPrice) {
-         this.setState({ priceInValid: true });
-         document.getElementById('carPriceInput').className += ' orange-boundary';
-         return false;
-      }
-   }
-   
-   resetValidationCSS = () => {
-      this.setState({ nameInValid: false, colorInValid: false, priceInValid: false})
-      document.getElementById('carPriceInput').className -= ' orange-boundary';
-      document.getElementById('carColorInput').className -= ' orange-boundary';
-      document.getElementById('carPriceInput').className -= ' orange-boundary';
-   }
-
-   updateCarApiCall = () => {
-      const patchURL = '/api/cars/' + this.props.match.params.id
+	updateCarApiCall = () => {
+		const patchURL = '/api/cars/' + this.props.match.params.id
 		axios.patch(patchURL, {
 			name: this.state.editedCarName,
 			color: this.state.editedCarColor,
@@ -102,65 +102,64 @@ class CarProfileComp extends React.Component{
 		}).then((res) => {
 			console.log(res);
 			this.callApiGetSingleCar().then( res => this.setState({
-            theSingleCar: res.singleCar,
-         })).catch( err => console.log(err))
+				theSingleCar: res.singleCar,
+			})).catch( err => console.log(err))
 		}).catch(err => console.log(err))
 	}
 
-   updateCar = () => {
-      this.resetValidationCSS()
-      console.log(this.state.editedCarName, this.state.editedCarColor, this.state.editedCarPrice)
-      this.validateFormData() && this.editModeOff()
-      this.updateCarApiCall()
-   }
+  updateCar = () => {
+    this.resetValidationCSS()
+    this.validateFormData() && this.editModeOff()
+    this.updateCarApiCall()
+  }
 
-   render(){
-      return(
-         <div className='col-md-10 col-md-offset-1'> 
-            <div className='col-sm-4'>
-               <h2> {this.state.theSingleCar.name} </h2>
-               <p> Color: <i className='fa fa-paint-brush'> </i> {this.state.theSingleCar.color} </p>
-               <p> Price: $ {this.state.theSingleCar.price }</p>
+  render(){
+    return(
+      <div className='col-md-10 col-md-offset-1'> 
+        <div className='col-sm-4'>
+          <h2> {this.state.theSingleCar.name} </h2>
+					<p> Color: <i className='fa fa-paint-brush'> </i> {this.state.theSingleCar.color} </p>
+					<p> Price: $ {this.state.theSingleCar.price }</p>
 
-               {! this.state.isEditing && 
-                  <div>
-                     <button className='btn btn-success' onClick={this.editModeOn}> Edit</button>
-                     <button className='btn btn-danger'>Delete</button>
-                  </div>
-               }
-            </div>
-            <div className='col-sm-8'>
-               { this.state.isEditing && 
-                  <div>
-                     <div className='col-sm-6'>
-                        <h3> {this.state.editedCarName}</h3>
-                        <p> {this.state.editedCarColor} {this.state.editedCarPrice}</p>
-                     </div>
-                     <div className='col-sm-6'>
-                        <h3> Edit Car</h3>
-                        <form className='form' >
-                           <label htmlFor='carName'> Name</label>
-                           <input type='text' className='form-control' name='carName' defaultValue={this.state.theSingleCar.name} onChange={this.handleCarNameChange} id='carNameInput' /> 
-                           {this.state.nameInValid && <span className='input-err'> ** Name Invalid</span>} <br/>
+					{! this.state.isEditing && 
+						<div>
+								<button className='btn btn-success' onClick={this.editModeOn}> Edit</button>
+								<button className='btn btn-danger'>Delete</button>
+						</div>
+					}
+        </div>
+				<div className='col-sm-8'>
+					{ this.state.isEditing && 
+						<div>
+							<div className='col-sm-6'>
+								<h3> {this.state.editedCarName}</h3>
+								<p> {this.state.editedCarColor} {this.state.editedCarPrice}</p>
+							</div>
+							<div className='col-sm-6'>
+								<h3> Edit Car</h3>
+								<form className='form' >
+									<label htmlFor='carName'> Name</label>
+									<input type='text' className='form-control' name='carName' defaultValue={this.state.editedCarName} onChange={this.handleCarNameChange} id='carNameInput' /> 
+									{this.state.nameInValid && <span className='input-err'> ** Name Invalid</span>} <br/>
 
-                           <label htmlFor='carColor'> Color</label>
-                           <input type='text' className='form-control' name='carColor' defaultValue={this.state.theSingleCar.color} onChange={this.handleCarColorChange} id='carColorInput' /> 
-                           {this.state.colorInValid && <span className='input-err'> ** Color Invalid</span>} <br/>
+									<label htmlFor='carColor'> Color</label>
+									<input type='text' className='form-control' name='carColor' defaultValue={this.state.editedCarColor} onChange={this.handleCarColorChange} id='carColorInput' /> 
+									{this.state.colorInValid && <span className='input-err'> ** Color Invalid</span>} <br/>
 
-                           <label htmlFor='carPrice'> Price</label>
-                           <input type='text' className='form-control' name='carPrice' defaultValue={this.state.theSingleCar.price} onChange={this.handleCarPriceChange} id='carPriceInput' /> 
-                           {this.state.priceInValid && <span className='input-err'> ** Price Invalid</span>}
+									<label htmlFor='carPrice'> Price</label>
+									<input type='text' className='form-control' name='carPrice' defaultValue={this.state.editedCarPrice} onChange={this.handleCarPriceChange} id='carPriceInput' /> 
+									{this.state.priceInValid && <span className='input-err'> ** Price Invalid</span>}
 
-                        </form>
-                        <button className='btn btn-primary' type='submit' onClick={this.updateCar}> Save</button>
-                        <button className='btn btn-default' onClick ={this.editModeOff}> Close</button>
-                     </div>
-                  </div>
-               }
-            </div>
-         </div>
-      );
-   }
+								</form>
+								<button className='btn btn-primary' type='submit' onClick={this.updateCar}> Save</button>
+								<button className='btn btn-default' onClick ={this.editModeOff}> Close</button>
+							</div>
+						</div>
+					}
+				</div>
+      </div>
+    );
+	} 
 }
 
 export default CarProfileComp;
--- react_node_express_app/src/components/fruitProfileComp.js
+++ react_node_express_app/src/components/fruitProfileComp.js
@@ -1,74 +1,90 @@
 import React from 'react';
 export default class FruitProfileComp extends React.Component{
-   constructor(props){
-      super(props);
-      this.state = {
-         theSingleFruit: {},
-         isEditing: false,
-         editedFruitName: "",
-         editedFruitWeight: "",
-         editedFruitFSize: ""
-      }
-      this.editModeOn = this.editModeOn.bind(this)
-      this.editModeOff = this.editModeOff.bind(this)
-   }
+  constructor(props){
+    super(props);
+    this.state = {
+      theSingleFruit: {},
+      isEditing: false,
+      editedFruitName: "",
+      editedFruitWeight: "",
+      editedFruitFSize: ""
+    }
+    this.editModeOn = this.editModeOn.bind(this)
+    this.editModeOff = this.editModeOff.bind(this)
+    this.handleFruitNameChange = this.handleFruitNameChange.bind(this)
+  }
 
-   componentDidMount(){
-      this.callApiGetSingleFruit().then( res => {
-         this.setState({ theSingleFruit: res.singleFruit })
-      }).catch(err => console.log(err))
-   } 
+  componentDidMount(){
+    this.callApiGetSingleFruit().then( res => { 
+      this.setState({ theSingleFruit: res.singleFruit })
+    }).catch(err => console.log(err))
+  } 
 
-   componentDidUpdate(){
-      this.callApiGetSingleFruit().then( res => {
-         this.setState({ theSingleFruit: res.singleFruit })
-      }).catch(err => console.log(err))
-   } 
+  componentDidUpdate(){
+    this.callApiGetSingleFruit().then( res => {
+      this.setState({ theSingleFruit: res.singleFruit })
+    }).catch(err => console.log(err))
+  } 
 
-   callApiGetSingleFruit = async() =>{
-      const fetchURL = '/api/fruits/' + this.props.match.params.id
-      const response = await fetch(fetchURL);
-      const resBody = await response.json();
-      if (response.status !== 200) throw Error(resBody.message);
-      return resBody;
-   }
+  callApiGetSingleFruit = async() =>{
+    const fetchURL = '/api/fruits/' + this.props.match.params.id
+    const response = await fetch(fetchURL);
+    const resBody = await response.json();
+    if (response.status !== 200) throw Error(resBody.message);
+    return resBody;
+  }
 
-   editModeOn = () => {
-      this.setState({ isEditing: true, editedFruitName: this.state.theSingleFruit.name, editedFruitWeight: this.state.theSingleFruit.weight, editedFruitFSize: this.state.theSingleFruit.fsize })
-   }
+  editModeOn = () => {
+    this.setState({ isEditing: true, editedFruitName: this.state.theSingleFruit.name, editedFruitWeight: this.state.theSingleFruit.weight, editedFruitFSize: this.state.theSingleFruit.fsize })
+  }
 
-   editModeOff = ()=> {
-      this.setState({ isEditing: false})
-   }
-   render(){
-      return(
-         <div className='col-md-10 col-md-offset-1'>
-            <div className='col-sm-4'>
-               <h2> {this.state.theSingleFruit.name} </h2>
-               <p> Weight: <i className='fa fa-paint-scale'> </i> {this.state.theSingleFruit.weight} </p>
-               <p> fSize: {this.state.theSingleFruit.fsize} px </p>
-               {! this.state.isEditing && 
-                  <div>
-                     <button className='btn btn-success' onClick={this.editModeOn}> Edit </button>
-                     <button className='btn btn-danger'> Delete </button>
-                  </div>
-               }
+  editModeOff = () => {
+    this.setState({ isEditing: false})
+  }
+
+  handleFruitNameChange = (event) => {
+    this.setState({ editedFruitName: event.target.value})
+  }
+
+  updateFruit = () => {
+    this.editModeOff()
+  }
+
+  render(){
+    return(
+      <div className='col-md-10 col-md-offset-1'>
+        <div className='col-sm-4'>
+            <h2> {this.state.theSingleFruit.name} </h2>
+            <p> Weight: <i className='fa fa-paint-scale'> </i> {this.state.theSingleFruit.weight} </p>
+            <p> fSize: {this.state.theSingleFruit.fsize} px </p>
+            {! this.state.isEditing && 
+              <div>
+                  <button className='btn btn-success' onClick={this.editModeOn}> Edit </button>
+                  <button className='btn btn-danger'> Delete </button>
+              </div>
+            }
+        </div>
+        <div className='col-sm-8'>
+          {this.state.isEditing && 
+            <div>
+              <div className='col-sm-6'>
+                <h3> {this.state.editedFruitName} </h3>
+                <p> {this.state.editedFruitWeight} {this.state.editedFruitFSize}</p>   
+              </div>
+              <div className='col-sm-6'>
+                <h3> Edit Car </h3>
+                <form className='form'>
+                    <label htmlFor='fruitName'> Name </label>
+                    <input type='text' className='form-control' name='fruitName' defaultValue={this.state.editedFruitName} onChange={this.handleFruitNameChange} id='fruitNameInput' />
+
+                    <button className='btn btn-primary' onClick={this.updateFruit}>Save</button>
+                    <button className='btn btn-default' onClick={this.editModeOff}>Cancel </button>
+                </form>
+              </div>
             </div>
-            <div className='col-sm-8'>
-               {this.state.isEditing && 
-                  <div>
-                     <div className='col-sm-6'>
-                        <h3> {this.state.editedFruitName} </h3>
-                        <p> {this.state.editedFruitWeight} {this.state.editedFruitFSize}</p>   
-                     </div>
-                     <div className='col-sm-6'>
-                        <h3> Edit Car </h3>
-                        <button className='btn btn-default' onClick={this.editModeOff}>Cancel </button>
-                     </div>
-                  </div>
-               }
-            </div>
-         </div>
-      )
-   }
+          }
+        </div>
+      </div>
+    )
+  }
 }
