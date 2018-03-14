120b1c80cc4f232f31bdf6ae4476e5aec77b94de
Toufiq <burningraven06@gmail.com>
2018-03-14 18:31:40 +0600

    create form validation on create car

--- react_node_express_app/src/components/carCreateFormComp.js
+++ react_node_express_app/src/components/carCreateFormComp.js
@@ -35,26 +35,26 @@ export default class CarCreateComp extends React.Component{
     }
     if (!this.state.carname.length > 0){
       this.setState({ nameInValid: true})
-      document.getElementById('carNameInput').className += ' orange-boundary';
+      document.getElementById('carNameInput').className += ' form-control orange-boundary';
       return false;
     }
     if (!this.state.carcolor.length > 0) {
       this.setState({ colorInValid: true })
-      document.getElementById('carColorInput').className += ' orange-boundary';
+      document.getElementById('carColorInput').className += ' form-control orange-boundary';
       return false;
     } 
     if (!this.state.carprice) {
       this.setState({ priceInValid: true })
-      document.getElementById('carPriceInput').className += ' orange-boundary';
+      document.getElementById('carPriceInput').className += ' form-control orange-boundary';
       return false;
     }
   }
 
   resetValidationCSS = () => {
     this.setState({ nameInValid: false, colorInValid: false, priceInValid: false })
-    document.getElementById('carPriceInput').className -= ' orange-boundary';
-    document.getElementById('carColorInput').className -= ' orange-boundary';
-    document.getElementById('carPriceInput').className -= ' orange-boundary';
+    document.getElementById('carNameInput').className -= 'orange-boundary';
+    document.getElementById('carColorInput').className -= 'orange-boundary';
+    document.getElementById('carPriceInput').className -= 'orange-boundary';
   }
 
   sendCarFormData = (event) => {
@@ -70,24 +70,26 @@ export default class CarCreateComp extends React.Component{
   render(){
     return (
       <div className='col-sm-6 col-sm-offset-3'> 
-        <form className='form'> 
-          <label htmlFor='carname'> Name</label>
-          <input type='text' placeholder='Name' name='carname' className='form-control' onChange={this.handleCarNameChange} id='carNameInput' /> 
-          {this.state.nameInValid && <span className='input-err'> ** Name Invalid</span> }
-            <br/>
-
-          <label htmlFor='carcolor'> Color</label> 
-          <input type='text' placeholder='Color' name='carcolor' className='form-control' onChange={this.handleCarColorChange} id='carColorInput' /> 
-          {this.state.colorInValid && <span className='input-err'> ** Color Invalid</span>}
-          <br />
-
-          <label htmlFor='carprice'> Price</label>
-          <input type='text' placeholder='Price' name='carprice' className='form-control' onChange={this.handleCarPriceChange} id='carPriceInput' /> 
-          {this.state.priceInValid && <span className='input-err'> ** Price Invalid</span>}<br />
-
-          <button type='submit' className='btn btn-primary' onClick={this.sendCarFormData}> Submit </button>
-          <button type='cancel' className='btn btn-default' onClick={this.unrenderCreateForm}> Cancel </button>
-
+        <form> 
+          <div className="form-group">
+            <label htmlFor='carname'> Name</label>
+            <input type='text' placeholder='Name' name='carname' className={'form-control'} onChange={this.handleCarNameChange} id='carNameInput' /> 
+            {this.state.nameInValid && <p className='input-err'> ** Name Invalid</p> }
+          </div>
+          <div className="form-group">
+            <label htmlFor='carcolor'> Color</label> 
+            <input type='text' placeholder='Color' name='carcolor' className={'form-control'} onChange={this.handleCarColorChange} id='carColorInput' /> 
+            {this.state.colorInValid && <p className='input-err'> ** Color Invalid</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor='carprice'> Price</label>
+            <input type='text' placeholder='Price' name='carprice' className={'form-control'} onChange={this.handleCarPriceChange} id='carPriceInput' /> 
+            {this.state.priceInValid && <p className='input-err'> ** Price Invalid</p>}<br />
+          </div>
+          <div className="form-group">
+            <button type='submit' className='btn btn-primary' onClick={this.sendCarFormData}> Submit </button>
+            <button type='cancel' className='btn btn-default' onClick={this.unrenderCreateForm}> Cancel </button>
+          </div>
         </form>
       </div>
     );
