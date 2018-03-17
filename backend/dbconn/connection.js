968d3dd91987964ff7d4b9b4389b73f468a15a15
Toufiq <burningraven06@gmail.com>
2018-03-17 22:18:30 +0600

    change dbUrl for heroku postgres env

--- backend/dbconn/connection.js
+++ backend/dbconn/connection.js
@@ -3,7 +3,8 @@ var pgpool = require('pg-promise')({
    promiseLib: blueBirdPromise
 });
 
-var connectionStr = "postgres://postgres:postgres@localhost:5432/carfruitdb";
+// var connectionStr = "postgres://postgres:postgres@localhost:5432/carfruitdb";
+var connectionStr = "postgres://ccvoombfeqheyi:74af532634ed52900af793bd6c6f3537d63b1289d86f92a8ee50ad11acc10cbb@ec2-54-163-246-193.compute-1.amazonaws.com:5432/d7egfmaeq2bj48";
 
 var db = pgpool(connectionStr);
 
